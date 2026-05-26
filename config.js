563ed07e521f67f9b4c35d790a2929f159b24fe5
Gittster <mattsweet147@gmail.com>
2026-05-26 13:15:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const CONFIG = {
   googleApiKey: "YOUR_API_KEY",
   minutesFolderId: "YOUR_MINUTES_FOLDER_ID",
   newsFolderId: "YOUR_NEWS_FOLDER_ID",
-  historyDocId: "YOUR_HISTORY_DOC_ID",
-  calendarId: "sweetsbordenlake@gmail.com"
+  historyDocId: "1z0TdBRUK-std645BIRb1zYH7IQKN25ITUttO-GoM0Wg",
+  calendarId: "sweetsbordenlakeassociation@gmail.com"
 };
